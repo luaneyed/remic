7bd56190d0405fd982359a5ca16417c3ae1274c0
Sangguk Lee <sangguk258@gmail.com>
2019-12-24 15:41:43 +0900

    Use clap to parse CLI arguments

--- client/src/bin/remic.rs
+++ client/src/bin/remic.rs
@@ -1,7 +1,30 @@
-use std::env;
+extern crate clap;
+use clap::{App, Arg, SubCommand};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("Remic")
+        .version("0.0.1")
+        .author("Sangguk Lee <sangguk258@gmail.com>")
+        .about("Remic Client CLI")
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("get value by key")
+                .arg(Arg::with_name("key").index(1).required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("set value to key")
+                .arg(Arg::with_name("key").index(1).required(true))
+                .arg(Arg::with_name("val").index(2).required(true)),
+        )
+        .get_matches();
 
-    println!("{}", remic::execute(&args[1..].join(" ")).unwrap());
+    let result = match matches.subcommand() {
+        ("get", Some(args)) => remic::get(args.value_of("key").unwrap()).unwrap(),
+        ("set", Some(args)) => {
+            remic::set(args.value_of("key").unwrap(), args.value_of("val").unwrap()).unwrap()
+        }
+        (&_, _) => panic!("Not supporting command"),
+    };
+    println!("{}", result);
 }
