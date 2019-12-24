5b843a2a71a03daa69d63899656a9f3f04d5bf16
Sangguk Lee <sangguk258@gmail.com>
2019-12-24 22:50:07 +0900

    Write test code of Store

--- server/src/storage/mod.rs
+++ server/src/storage/mod.rs
@@ -19,3 +19,40 @@ impl Store {
         self.map.get(key)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn set_get() {
+        let mut store = Store::new();
+
+        store.set("key1".to_string(), "value1".to_owned());
+        store.set("key2".to_owned(), "value2".to_owned());
+
+        assert_eq!(store.get("key1"), Some(&"value1".to_owned()));
+        assert_eq!(store.get("key2"), Some(&"value2".to_owned()));
+    }
+
+    // Should overwrite existent value
+    #[test]
+    fn overwrite_set() {
+        let mut store = Store::new();
+
+        store.set("key1".to_owned(), "value1".to_owned());
+        assert_eq!(store.get("key1"), Some(&"value1".to_owned()));
+
+        store.set("key1".to_owned(), "value2".to_owned());
+        assert_eq!(store.get("key1"), Some(&"value2".to_owned()));
+    }
+
+    // Should get `None` when getting a non-existent key
+    #[test]
+    fn get_non_existent_value() {
+        let mut store = Store::new();
+
+        store.set("key1".to_owned(), "value1".to_owned());
+        assert_eq!(store.get("key2"), None);
+    }
+}
